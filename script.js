f9082d6ae44e70a844269d69c7935df36a5b5140
amanda-bann <amanda_klap@protonmail.com>
2024-09-30 13:10:53 -0600

    Shout it exercise

---
+++ script.js
@@ -0,0 +1,8 @@
+var shoutIt = function(name) {
+    var shout = `Hello, ${name}, pleasure to meet you!`.toUpperCase();
+    return shout;
+};
+
+console.log(shoutIt("Chloe"));
+console.log(shoutIt("Tek"));
+console.log(shoutIt("Banner"));
